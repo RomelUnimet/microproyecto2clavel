3a8ec2b365e2e241ca7188855fd396424f507dc8
Romel Clavel <romeletoty@gmail.com>
2019-11-02 08:38:58 -0400

    as

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase:{
-    apiKey: "AIzaSyCLpa7qyCdUWASLJHNyA009sWHD3lc9kyQ",
-    authDomain: "microproyecto-2-romel-clavel.firebaseapp.com",
-    databaseURL: "https://microproyecto-2-romel-clavel.firebaseio.com",
-    projectId: "microproyecto-2-romel-clavel",
-    storageBucket: "microproyecto-2-romel-clavel.appspot.com",
-    messagingSenderId: "262794276662",
-    appId: "1:262794276662:web:1f4e84f5344b52bb994d97"
-  }
+  
 
 };
 
